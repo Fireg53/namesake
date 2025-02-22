f9e0ff0ecbae4c3909d0836aa4326328386a7ab6
Iyan SM <iyansyeedmiller@gmail.com>
2025-02-22 12:46:12 -0700

    customing vue defualts

--- namesake/src/App.vue
+++ namesake/src/App.vue
@@ -1,20 +1,13 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
+import ToDoItem from "./components/ToDoItem.vue";
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div>
+    <h1>To-Do List</h1>
+    <to-do-item></to-do-item>
+  </div>
 </template>
 
 <style scoped>
